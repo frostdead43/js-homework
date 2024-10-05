edfe32c641a31a4e0a975b23b2d7ea1c5b0c7033
Mehmet Akif  Kücükyilmazz <makif4307@hotmail.com>
2024-10-05 16:09:07 +0300

    rock-scissors-paper yapildi.Hikaye'nin ilk kismi tamamlandi

---
+++ assets/story.js
@@ -0,0 +1,39 @@
+let adventurerName = prompt("İsmini yaz!");
+
+let adventurerPower = Math.floor(Math.random() * (10 - 6 + 1)) + 6;
+
+let bandits = "Haydutlar";
+
+let banditPower = Math.floor(Math.random()* 6);
+
+let path1 = prompt("Tüm gün boyunca at sürdüğün için kendini yorgun hissediyorsun. Uzaklarda bir köyden dumanlar çiktigini gördün. Bu senin için dinlenme firsati olabilir. Köye dogru atini sürmek istiyor musun? ")
+
+if (path1 === "evet") {
+    console.log("köye dogru ilerlemeye karar verdin. Köye yaklastikca bir seylerin ters gittiğini anliyorsun. Atin huysuzlanmaya basladi. Köye girdiginde harsilastigin manzara korkunc. Yikilmis evler, üst üste yigilmis ceset yiginlari bu köyün ne hale geldiğini sana gösteriyor. Köyün meydaninda asilmis yasli bir adam var. Bu adam köyün lideri olmali. Ölmeden önce iskence görmüs. Vücudunda bicak yaralari var. Bu yaralar canavarlar tarafindan yapilmisa benzemiyor. Köyün icinde canli belirtisi var mi diye inceliyorsun. Köyün arkalarinda bagirisma sesleri duyuyorsun. Silahini yanina alip seslerin oldugu bölgeye gitme karari aldin!")
+
+    path1_2 = prompt("Seslerin geldigi yöne geldiginde bu köyün neden bu hale geldigini anliyorsun. Haydutlar! Haydutlar önünde genc bir kiza saldiriyor. 'Bir tanesi seni farkediyor: Sayimiz fazla. Bela istemiyorsan buradan uzaklas!' Genc kiz yardim istercesine sana bakiyor, ne yapacaksin?`");
+
+    if (path1_2 === "saldir") {
+        if (adventurerPower > banditPower) {
+                console.log("Son haydutu katlettiginde ölmeden önce sana bakiyor." + `Seni hatirladim, sen ${adventurerName} 'sin. Haydutun göz bebekleri yavas yavas büyürken sen de genc kizin iyi oldugunu düsünüp atina dogru ilerliyorsun. Köyden ayrilip sehire dogru yola cikiyorsun. Umarim yolda yiyecek bulabilirsin`);
+        }else if (banditPower >= adventurerPower) {
+                console.log("Ne kadar güclü olsan da haydurlarin sayisi karsisinda bir sansin kalmiyor.");
+        }else 
+            alert("gecersiz hamle");
+
+    } else if (path1_2 === "saldirma") {
+        console.log ("Kendini düsünmek zorundasin. Dünya acimasiz. Genc kizi kaderiyle başbaşa birakiyorsun Köyden ayriliyorsun" );
+    } else 
+        alert("Lütfen gecerli bir hamle yap!")
+
+} else if (path1 === "hayir") {
+
+    console.log("Atini kuzeye ceviriyorsun. Şehre ulasmana az kalmis olabilir. Şu an dinlenmenin sirasi değil!");
+
+    prompt("sehre geldin.")
+
+} else {
+    alert ("Lütfen gecerli bir hamle yap!")
+} 
+
+
