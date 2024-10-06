402618acd3d894498686a1c7fa26824538ef1701
Mehmet Akif  Kücükyilmazz <makif4307@hotmail.com>
2024-10-06 18:24:36 +0300

    2 question tamamlandi

---
+++ assets/questions.js
@@ -0,0 +1,52 @@
+// Basit İndirim Hesaplama:
+// Bir ürünün fiyatını ve indirim yüzdesini kullanıcıdan alarak, indirimli fiyatı hesaplayan bir program yazın.
+// Örn: 100 TL olan bir ürüne %20 indirim yapıldığında ne kadar ödenmesi gerektiğini hesaplayıp alert ile gösterin.
+
+
+
+
+// let price = prompt("Enter price:");
+
+// let discount = prompt("Enter discount rate(with % ):");
+
+// let sale = ((price * discount) / 100);
+
+// let lastPrice = price - sale;
+
+// if (lastPrice !== 0) {
+//     alert("Product Price:" + " "  + lastPrice );
+// }else (lastPrice === "0" || " ") 
+//     alert("0 or invalid!")
+
+
+// Vize ve Final Notu ile Ortalama:
+// Kullanıcıdan vize ve final notunu alarak dönem ortalamasını hesaplayan bir program yazın (vize %40, final %60 etkili olsun).
+// Eğer not 50’den büyükse "Geçtiniz", 50'den küçükse ve 40’tan büyükse "Şartlı geçtiniz", 40’tan küçükse "Kaldınız" mesajını verin.
+
+
+// let vize = prompt("first exam: ");
+
+// let final = prompt("final exam: ");
+
+// let average = (((vize * 40) / 100) + ((final * 60)/100));
+
+
+// if (average >= 50) {
+//     alert(average);
+//     alert("You passed this exam!");
+// } else if (average < 50 && average >= 40) {
+//     alert(average);
+//     alert("You have conditionally passed this exam!");
+// } else if (average < 40){
+//     alert(average);
+//     alert("You failed!");
+
+// } else alert("0 or invalid!"); 
+
+
+
+    
+
+
+
+
